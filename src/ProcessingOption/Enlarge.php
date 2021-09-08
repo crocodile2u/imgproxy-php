e43dbf38b945837c9baa7fe3dd658d985a557849
Victor Bolshov <vbolshov@harlemnext.com>
2021-09-08 13:29:59 +0200

    add Enlarge

---
+++ src/ProcessingOption/Enlarge.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imgproxy\ProcessingOption;
+
+use Imgproxy\ProcessingOption;
+
+class Enlarge extends ProcessingOption
+{
+    protected $name = ProcessingOption::ENLARGE;
+}
