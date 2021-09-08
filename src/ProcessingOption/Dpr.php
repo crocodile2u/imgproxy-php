6688a881738e7be375c2d5ddc91a931f2bb74386
Victor Bolshov <vbolshov@harlemnext.com>
2021-09-08 13:22:57 +0200

    add Dpr

---
+++ src/ProcessingOption/Dpr.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imgproxy\ProcessingOption;
+
+use Imgproxy\ProcessingOption;
+
+class Dpr extends ProcessingOption
+{
+    protected $name = ProcessingOption::DPR;
+}
